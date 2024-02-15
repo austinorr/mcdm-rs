ed624301c865020e21219a47b260306d69a9ce72
Austin Orr <aorr@geosyntec.com>
2024-02-14 17:21:35 -0800

    make bench more representative

--- benches/benchmarks/mc_bench.rs
+++ benches/benchmarks/mc_bench.rs
@@ -1,6 +1,6 @@
 use criterion::{criterion_group, Criterion};
 use mcdmrs::prom::multicriterion_flow;
-use mcdmrs::prom::types::{Fl, Mat};
+use mcdmrs::prom::types::{Arr, Fl, Mat};
 use rand::{distributions::Uniform, Rng};
 
 pub fn mc_bench(c: &mut Criterion) {
@@ -14,14 +14,12 @@ pub fn mc_bench(c: &mut Criterion) {
         matrix_t.push((0..n).map(|_| 20.0 * rng.sample(&range)).collect())
     }
 
-    let len: usize = matrix_t.len();
-
     let pref = ["usual", "ushape", "vshape", "vshape2", "level"]
         .map(String::from)
         .to_vec();
 
-    let q: Vec<Fl> = vec![0.; len];
-    let p: Vec<Fl> = vec![0.; len];
+    let q: Arr = vec![0.37, 0.95, 0.73, 0.6, 0.16];
+    let p: Arr = vec![0.6, 0.71, 0.02, 0.97, 0.83];
 
     c.bench_function("n6000_m5", |b| {
         b.iter(|| multicriterion_flow(&matrix_t, &pref, &q, &p))
