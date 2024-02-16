cac99645975d8e1b3438d1089b2e140b5cfe9491
Austin Orr <aorr@geosyntec.com>
2024-02-16 15:05:45 -0800

    add polars support and cli

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None, arg_required_else_help = true)]
+struct Cli {
+    /// The path to the file to read
+    #[arg(short, long)]
+    data: std::path::PathBuf,
+
+    #[arg(short, long)]
+    criteria: std::path::PathBuf,
+}
+
+fn main() {
+    fn configure_the_environment() {
+        use std::env;
+        env::set_var("POLARS_FMT_TABLE_ROUNDED_CORNERS", "1"); // apply rounded corners to UTF8-styled tables.
+        env::set_var("POLARS_FMT_MAX_COLS", "20"); // maximum number of columns shown when formatting DataFrames.
+        env::set_var("POLARS_FMT_MAX_ROWS", "50"); // maximum number of rows shown when formatting DataFrames.
+        env::set_var("POLARS_FMT_STR_LEN", "50"); // maximum number of characters printed per string value.
+    }
+
+    if cfg!(feature = "io") && cfg!(feature = "cli") {
+        use mcdmrs::prom::{interop, Prom};
+        use polars::prelude::*;
+
+        let args: Cli = Cli::parse();
+        println!("{:#?}", args);
+
+        let mut data_df = interop::polars::df_from_csv(
+            args.data
+                .to_str()
+                .expect("failed to convert data path to str"),
+        )
+        .expect("failed to load data");
+        let criteria_df: DataFrame = interop::polars::df_from_csv(
+            args.criteria
+                .to_str()
+                .expect("failed to convert criteria path to str"),
+        )
+        .expect("failed to criteria");
+
+        let mut p: Prom = interop::polars::prom_from_polars(&data_df, &criteria_df).unwrap();
+        _ = p.compute_prom_ii().expect("failed to compute prom.");
+
+        let score = Series::new("score", p.prom_ii.as_ref().unwrap().score.clone());
+        data_df.with_column(score).unwrap();
+
+        configure_the_environment();
+        println!("Scoring Criteria {:#?}", criteria_df);
+        println!("Data with Prom II Scores {:#?}", data_df);
+    } else {
+        println!("`cli` and `io` feature are required for command line use.")
+    }
+}
