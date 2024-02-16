d7b329b296ee4c82ee756cbbb4ab6576fde9ef38
Austin Orr <aorr@geosyntec.com>
2024-02-16 15:05:06 -0800

    refactor prom criteria

--- src/prom/mod.rs
+++ src/prom/mod.rs
@@ -2,6 +2,7 @@ pub mod matrix;
 mod multicriterion_flow;
 pub use multicriterion_flow::multicriterion_flow;
 mod prom;
+pub use prom::{Criteria, Prom};
 pub use prom::Prom;
 mod pref_functions;
 pub mod types;
