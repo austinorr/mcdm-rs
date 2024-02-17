ce031f4b35891167afc4d46cfe85306bf5cc6441
Austin Orr <aorr@geosyntec.com>
2024-02-17 13:32:56 -0800

    add integration tests

---
+++ tests/cli.rs
@@ -0,0 +1,28 @@
+use assert_cmd::prelude::*;
+use std::process::Command; // Run programs // Add methods on commands
+
+#[test]
+fn test_happy_path() {
+    let mut cmd = Command::cargo_bin("mcdmrs").expect("executable not found");
+
+    cmd.arg("--alternatives")
+        .arg("./examples/data/alternatives.csv")
+        .arg("--criteria")
+        .arg("./examples/data/criteria.csv");
+
+    cmd.assert().success();
+}
+
+#[test]
+fn test_file_doesnt_exist_buffer() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("mcdmrs")?;
+
+    cmd.arg("--alternatives")
+        .arg("test/file/doesnt/exist")
+        .arg("--criteria")
+        .arg("test/file/doesnt/exist");
+
+    cmd.assert().failure();
+
+    Ok(())
+}
